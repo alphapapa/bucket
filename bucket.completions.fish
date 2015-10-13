f9b587fdb24f403f3495797ab3681ab5174b70dd
Adam Porter <adam@alphapapa.net>
2015-10-12 19:29:00 -0500

    Add Fish completions

---
+++ bucket.completions.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+
+complete -c bucket -Af -s a -l append
+complete -c bucket -Af -s e -l empty
+complete -c bucket -Af -s g -l grep
+complete -c bucket -Af -s h -l help
+complete -c bucket -Af -s l -l list
+complete -c bucket -Af -s v -l verbose
+complete -c bucket -Af -s V -l VERBOSE
+complete -c bucket -Af -s x -l expire
+complete -c bucket -f -a '(ls ~/.cache/bucket)'
